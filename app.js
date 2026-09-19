92611b0cd2c98fed76138cbf41bfdbb660e2aa26
zhanglyt <zhanglyt@example.com>
2026-09-19 16:38:35 +0800

    修复启动方式

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { NeteaseCloudMusicApi } = require('NeteaseCloudMusicApi');
-const PORT = process.env.PORT || 3000;
+const { serve } = require('NeteaseCloudMusicApi')
+const PORT = process.env.PORT || 3000
 
-NeteaseCloudMusicApi.serve(PORT, '0.0.0.0').then(() => {
-  console.log(`Server running on port ${PORT}`);
-});
+serve(PORT, '0.0.0.0').then(() => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`)
+})
